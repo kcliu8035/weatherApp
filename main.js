260a93668990055db5b0bf270ca4341d485b94b9
Kevin Liu <134642275+kcliu8035@users.noreply.github.com>
2024-07-16 05:51:52 -0400

    Changed API server. Set up majority of functionality of getting city input and displaying relevant data.

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const temp = document.querySelector('.temp');
 const condition = document.querySelector('.condition');
 const conditionIcon = document.querySelector('.conditionIcon');
-let cityInput = "tokyo";
+const cityText = document.querySelector('.cityText');
+const timeField = document.querySelector('.timeField');
+// const OLDURL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=3LMRC5HLSRGT53CBJQ6ZM26QQ&contentType=json`
+
 
 // const URL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${cityInput}?unitGroup=metric&key=3LMRC5HLSRGT53CBJQ6ZM26QQ&contentType=json`
 
@@ -20,8 +23,9 @@ let cityInput = "tokyo";
 // });
 
 
-async function getData() {
-    const URL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${cityInput}?unitGroup=metric&key=3LMRC5HLSRGT53CBJQ6ZM26QQ&contentType=json`
+async function getData(city) {
+    let URL = `https://api.weatherapi.com/v1/current.json?key=3879078affd243f5b1005402242306&q=${city}&aqi=no`
+
     try {
       const response = await fetch(URL);
       if (!response.ok) {
@@ -30,16 +34,21 @@ async function getData() {
   
       const json = await response.json();
       console.log(json);
-      alert(json.resolvedAddress);
-      alert(json.currentConditions.temp);
-      alert(json.currentConditions.conditions);
-      alert(json.currentConditions.icon);
-      conditionIcon.src = json.currentConditions.icon;
-
+      cityText.innerHTML = `${json.location.name}`;
+      timeField.innerHTML = json.location.localtime;
+      temp.innerHTML = `${json.current.temp_f} °F`;
+      condition.innerHTML = json.current.condition.text;
+      conditionIcon.src = json.current.condition.icon;
 
     } catch (error) {
       console.error(error.message);
     }
+}
+
+function getCity(city) {  
+    let cityInput = document.querySelector('.inputField').value;
+    city = cityInput;
+    getData(city);
 }
 
 getData();
