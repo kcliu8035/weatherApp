f1049548480b16393383ce361133d3403479bed6
Kevin Liu <134642275+kcliu8035@users.noreply.github.com>
2024-07-24 05:40:32 -0400

    Added functionality to grab user's current location to display relevant weather information. Converted military time to standard.

--- main.js
+++ main.js
@@ -8,17 +8,17 @@ let latitude = "";
 
 
 
-function getLocation() {
+function getCurrentLocation() {
 
   function success(position) {
     console.log(position);
     latitude = position.coords.latitude;
     longitude = position.coords.longitude;
-    console.log(latitude.toFixed(2));
-    console.log(longitude);
+    // console.log(latitude.toFixed(2));
+    // console.log(longitude);
     let lon = longitude.toFixed(2);
     let lat = latitude.toFixed(2);
-    current(longitude, latitude);
+    renderCurrentLocation(longitude, latitude);
   }
 
   function error() {
@@ -28,14 +28,15 @@ function getLocation() {
   navigator.geolocation.getCurrentPosition(success, error);
 }
 
-document.addEventListener('DOMContentLoaded', getLocation());
+document.addEventListener('DOMContentLoaded', getCurrentLocation());
 
 
-function current(lon, lat) {
-  let apiUrl = `https://api.weatherapi.com/v1/current.json?key=3879078affd243f5b1005402242306&q=data?lon=${lon}&lat=${lat}`;
-  console.log(apiUrl);
+// async function current(lon, lat) {
+//   let apiURL = `https://api.weatherapi.com/v1/current.json?key=3879078affd243f5b1005402242306&q=data?lon=${lon}&lat=${lat}`;
+//   console.log(apiURL);
 
-}
+// }
+
 
 async function getData(city) {
     let URL = `https://api.weatherapi.com/v1/current.json?key=3879078affd243f5b1005402242306&q=${city}&aqi=no`
@@ -63,6 +64,42 @@ function getCity(city) {
 }
 
 
+
+async function renderCurrentLocation(lon, lat) {
+  // let apiURL = `https://api.weatherapi.com/v1/current.json?key=3879078affd243f5b1005402242306&q=data?lon=${lon}&lat=${lat}`;
+  let apiURL = `https://api.weatherapi.com/v1/current.json?key=3879078affd243f5b1005402242306&q=${lat},${lon}`;
+  try {
+    const response = await fetch(apiURL, {mode: 'cors'});
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    const json = await response.json();
+    console.log(json);
+    cityText.innerHTML = `${json.location.name}`;
+
+    var militaryTime = json.location.localtime.split(' ')[1].replace(':', '');
+    let jsonTime = convertMilitaryToStandardTime(militaryTime);
+    timeField.innerHTML = jsonTime;
+    temp.innerHTML = `${json.current.temp_f} °F`;
+    condition.innerHTML = json.current.condition.text;
+    conditionIcon.src = json.current.condition.icon;
+  } catch (error) {
+    console.error(error.message);
+  }
+  
+  console.log(apiURL);
+
+}
+
+function convertMilitaryToStandardTime(militaryTime) {
+  var hours = parseInt(militaryTime.substring(0, 2), 10);
+  var minutes = militaryTime.substring(2, 4);
+  var period = (hours >= 12) ? "PM" : "AM";
+  hours = (hours > 12) ? hours - 12 : hours;
+  hours = (hours == 0) ? 12 : hours;
+  minutes = (minutes < 10 ? '0' : '') + minutes;
+  return hours + ":" + minutes + " " + period;
+}
 
 
 // const OLDURL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=3LMRC5HLSRGT53CBJQ6ZM26QQ&contentType=json`
